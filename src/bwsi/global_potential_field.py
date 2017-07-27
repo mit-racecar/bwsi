9b8f8774ac12e55720673284938374f120d41c95
Ariel Anders <aanders@mit.edu>
2017-07-27 13:30:48 -0400

    some reason commandi s off

--- src/bwsi/global_potential_field.py
+++ src/bwsi/global_potential_field.py
@@ -10,24 +10,141 @@ potential fields through RVIZ.
 """
 
 from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Pose, Vector3, Quaternion, Point, PointStamped
+from geometry_msgs.msg import Pose, PoseStamped, Vector3, \
+        Quaternion, Point, PointStamped
 from std_msgs.msg import Header,ColorRGBA
 import rospy
 import numpy as np
 import cv_bridge
 from itertools import product
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from ackermann_msgs.msg import AckermannDriveStamped as Drive
 
 to_quat =lambda a:Quaternion(*quaternion_from_euler(0,0,a))
 to_angle=lambda q:euler_from_quaternion((q.x,q.y,q.z,q.w))[2]
 to_pose=lambda x,y,th:Pose(Point(x,y,0),to_quat(th))
 
+class ARMap:
+    def __init__(self, ground, obstacles, ar_tags):
+        self.GROUND = ground
+        self.OBSTACLES = obstacles
+        self.AR_TAGS = ar_tags
+        self.map_pub = rospy.Publisher('simple_map', MarkerArray, queue_size=1)
+        self.vf_pub = rospy.Publisher('vector_field', MarkerArray, queue_size=1)
+        self.COLORS = {'ground': (1,1,1,1), 'obs':(.3,.3,.3,1), 'ar':(0,0,0,1)}
+        self.THICKNESS = {'ground':.0001, 'obs':1, 'ar':.2}
+        rospy.sleep(.5)
+        self.drawMap()
+        rospy.loginfo("Done initializing global potential field.  View RVIZ ")
+    
+    """
+    ================  Drawing functions below =====================
+    """
+    
+    def clearMarkers(self, maps=False, vector=False):
+        ma = MarkerArray()
+        ma.markers= [self.stamp(Marker())]
+        ma.markers[0].action = 3
+        ma.markers[0].ns="maps"
+        if maps: self.map_pub.publish(ma)
+        if vector: self.vf_pub.publish(ma)
+
+    def drawMap(self):
+        self.clearMarkers(maps=True)
+        ma = MarkerArray()
+        ma.markers = [self.drawRect('ground',0,0,0, *self.GROUND)]
+        for i,o in enumerate(self.OBSTACLES):
+            ma.markers.append(self.drawRect('obs',i,*o))
+        for i, (x,y,th) in enumerate(self.AR_TAGS):
+            ma.markers.append(self.drawRect('ar', i, x,y,.1,.1))
+        self.map_pub.publish(ma)
+    
+    def drawVectorField(self, potential_func, goal=None):
+        self.clearMarkers(vector=True)
+        ma = MarkerArray()
+        x = np.linspace(0.05, self.GROUND[0], 30)
+        y = np.linspace(0.05, self.GROUND[1], 30)
+
+        for pt in product(x,y):
+            r,th = potential_func(pt)
+            ma.markers.append(self.drawArrow(pt[0],pt[1],r,th))
+        if goal is not None:
+            ma.markers.append(self.drawGoal(goal))
+        self.vf_pub.publish(ma)
+
+    def stamp(self, msg):
+        msg.header = Header(0, rospy.Time.now(), 'map')
+        return msg
+
+    def drawArrow(self, x, y, r, th):
+        r = max(.2, min(1, r *0.01))
+        m = self.stamp(Marker())
+        m.id = hash("%s%s"%(x,y))%10000
+        m.type = 0
+        m.action = 0
+        m.ns = "maps"
+        x2,y2 = x+ r*np.cos(th), y+r*np.sin(th)
+        m.points =[ Point(x,y,0), Point(x2,y2,0)]
+        m.scale=Vector3(.025, .05, 0.1)
+        m.color = ColorRGBA(1,0,0,1)
+        return m
+
+    def drawGoal(self, goal):
+        m = self.stamp(Marker())
+        m.pose = to_pose(goal[0], goal[1], 0)
+        m.type = 2
+        m.color = ColorRGBA(0,1,0,1)
+        m.scale = Vector3(.5,.5,.5)
+        m.ns ="maps"
+        m.id=hash('goal')%1000
+        return m
+
+    def drawRect(self, name, index, x, y, w, h):
+        m = self.stamp(Marker())
+        m.id =hash(name+str(index))%100
+        m.type = 1
+        m.action = 0
+        m.ns = "maps"
+        m.pose = to_pose(x+w/2.,y+h/2.,0)
+        z = self.THICKNESS[name]
+        m.scale = Vector3(w,h,z)
+        m.color = ColorRGBA(*self.COLORS[name])
+        return m
+
+    def clearMarkers(self, maps=False, vector=False):
+        ma = MarkerArray()
+        ma.markers= [self.stamp(Marker())]
+        ma.markers[0].action = 3
+        ma.markers[0].ns="maps"
+        if maps: self.map_pub.publish(ma)
+        if vector: self.vf_pub.publish(ma)
+
+   
+
+
+
 class GlobalPotentialField:
     K_a = 1  # gain for attractive potentials
-    K_r = 2  # gain for repulsive potentials
-    Obs_ignore = .75  #if car is more than .75m away from vehicle ignore it
+    K_r = 3 # gain for repulsive potentials
+    Obs_ignore = 2  #if car is more than .75m away from vehicle ignore it
     Obs_gamma = 2   # factor for how much we care about obstacles 
 
+    def __init__(self, armap):
+        self.goal = None
+        self.armap=armap
+        self.LINES = [self.lineSegments(*obs) for obs in armap.OBSTACLES]
+        self.LINES += [self.lineSegments(0,0,*armap.GROUND)]
+        self.goal_sub = rospy.Subscriber('clicked_point', \
+                PointStamped,self.goalCB,queue_size=1)
+        self.armap.drawVectorField(self.potential, self.goal)
+        rospy.loginfo("Done initializing global potential field.  View RVIZ ")
+    
+    def goalCB(self, msg):
+        self.goal = np.array([msg.point.x, msg.point.y])
+        rospy.loginfo( "updated goal to %s" % self.goal )
+        self.armap.drawVectorField(self.potential, self.goal)
+    
+    
     def potential_attract(self, pt):
         """
         #XXX For students to do
@@ -92,39 +209,6 @@ class GlobalPotentialField:
 
 
     """
-    ========================================================================
-    The remaining program has functions for computing distances to polygons,
-    drawing with rviz, and subscribing to a clicked point.
-    For this assignment you do not have to modify the functions below,
-    but you are of course welcome to.
-    ========================================================================
-    """
-    def __init__(self, ground, obstacles, ar_tags):
-        self.goal = None
-        self.GROUND = ground
-        self.OBSTACLES = obstacles
-        self.AR_TAGS = ar_tags
-        self.map_pub = rospy.Publisher('simple_map', MarkerArray, queue_size=1)
-        self.vf_pub = rospy.Publisher('vector_field', MarkerArray, queue_size=1)
-        self.LINES = [self.lineSegments(*obs) for obs in obstacles]
-        self.LINES += [self.lineSegments(0,0,*self.GROUND)]
-        self.COLORS = {'ground': (1,1,1,1), 'obs':(.3,.3,.3,1), 'ar':(0,0,0,1)}
-        self.THICKNESS = {'ground':.0001, 'obs':1, 'ar':.2}
-
-        rospy.sleep(.5)
-        self.drawMap()
-        self.drawVectorField()
-        self.goal_sub = rospy.Subscriber('clicked_point', \
-                PointStamped,self.goalCB,queue_size=1)
-        rospy.loginfo("Done initializing global potential field.  View RVIZ ")
-    
-    def goalCB(self, msg):
-        self.goal = np.array([msg.point.x, msg.point.y])
-        rospy.loginfo( "updated goal to %s" % self.goal )
-        self.drawVectorField()
-    
-
-    """
     ==========Functions for computing distance to obstacles  =========
     """
 
@@ -160,81 +244,82 @@ class GlobalPotentialField:
         lines = [(points[i-1], points[i])for i in range(len(points))]
         return np.array(lines, dtype=np.float32)
 
-    """
-    ================  Drawing functions below =====================
-    """
- 
-    def stamp(self, msg):
-        msg.header = Header(0, rospy.Time.now(), 'map')
-        return msg
 
-    def drawVectorField(self):
-        self.clearMarkers(vector=True)
-        ma = MarkerArray()
-        x = np.linspace(0.05, self.GROUND[0], 30)
-        y = np.linspace(0.05, self.GROUND[1], 30)
+class CarControl:
+    K_speed = 1
+    K_angle = 1
+    def __init__(self, gpf):
+        self.gpf = gpf
+        self.iter = 0
+        self.cmd = None
+        self.pub_cmd = rospy.Publisher("/vesc/ackermann_cmd_mux/input/teleop", Drive, queue_size=1)
 
-        for pt in product(x,y):
-            r,th = self.potential(pt)
-            ma.markers.append(self.drawArrow(pt[0],pt[1],r,th))
-        if self.goal is not None:
-            ma.markers.append(self.drawGoal())
-        self.vf_pub.publish(ma)
+        self.pub_vis = rospy.Publisher("potential_field_cmd", \
+                PoseStamped, queue_size=1)
+        #XXX using simulatorself.sub_pose = rospy.Subscriber('/expected_pose', \
+        self.sub_pose = rospy.Subscriber('/mle_pose', \
+                PoseStamped, self.cbPose,queue_size=1)
+        self.run()
     
-    def drawArrow(self, x, y, r, th):
-        r = max(.2, min(1, r *0.01))
-        m = self.stamp(Marker())
-        m.id = hash("%s%s"%(x,y))%10000
-        m.type = 0
-        m.action = 0
-        m.ns = "maps"
-        x2,y2 = x+ r*np.cos(th), y+r*np.sin(th)
-        m.points =[ Point(x,y,0), Point(x2,y2,0)]
-        m.scale=Vector3(.025, .05, 0.1)
-        m.color = ColorRGBA(1,0,0,1)
-        return m
+    def run(self):
+        while not rospy.is_shutdown():
+            if self.cmd is not None:
+                self.pub_cmd.publish(self.cmd)
+            rospy.sleep(0.01)
 
-    def drawGoal(self):
-        m = self.stamp(Marker())
-        m.pose = to_pose(self.goal[0], self.goal[1], 0)
-        m.type = 2
-        m.color = ColorRGBA(0,1,0,1)
-        m.scale = Vector3(.5,.5,.5)
-        m.ns ="maps"
-        m.id=hash('goal')%1000
-        return m
+    def cbPose(self, msg):
+            
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = to_angle(msg.pose.orientation)
+        
+        potential = self.gpf.potential((x,y))
+        
+        angle = np.arctan2(potential[1],potential[0]) - z
+
+        debug = ""
+        if np.abs(angle) > np.pi/2.:
+            angle = -angle
+            speed = -1
+            debug += "go backwards"
+        else:
+            speed = 1
+
+        msg.pose.orientation = to_quat(angle)
+        self.pub_vis.publish(msg)
+        dr = Drive()
+        dr.header = msg.header
+
+                    
+        dr.drive.steering_angle = self.K_angle*(angle)
+        dr.drive.speed = self.K_speed*speed
+        if False : #potential[0]==0 and potential[1] == 0:
+            dr.drive.steering_angle = 0
+            dr.drive.speed = 1
+            self.cmd = dr
+
+            debug += "No potential value"
+        elif x < 0 or y < 0 or x > self.gpf.armap.GROUND[0] or y > self.gpf.armap.GROUND[1]:
+            self.cmd = None
+            debug += "Car is out of bounds"
+        else:
+            self.cmd = dr
 
 
-    def drawRect(self, name, index, x, y, w, h):
-        m = self.stamp(Marker())
-        m.id =hash(name+str(index))%100
-        m.type = 1
-        m.action = 0
-        m.ns = "maps"
-        m.pose = to_pose(x+w/2.,y+h/2.,0)
-        z = self.THICKNESS[name]
-        m.scale = Vector3(w,h,z)
-        m.color = ColorRGBA(*self.COLORS[name])
-        return m
 
-    def clearMarkers(self, maps=False, vector=False):
-        ma = MarkerArray()
-        ma.markers= [self.stamp(Marker())]
-        ma.markers[0].action = 3
-        ma.markers[0].ns="maps"
-        if maps: self.map_pub.publish(ma)
-        if vector: self.vf_pub.publish(ma)
+        self.iter = (self.iter +1) % 1000
+        if self.iter%10 == 0:
+            #print  (x,y,z), 
+            if dr.drive.steering_angle == 0:
+                pass
+            elif dr.drive.steering_angle > 0:
+                debug += " left"
+            else:
+                debug += " right"
+            print "(%.2f, %.2f), %.2f %s" %( potential[0], potential[1], angle*180./np.pi, debug)
 
-    def drawMap(self):
-        self.clearMarkers(maps=True)
-        ma = MarkerArray()
-        ma.markers = [self.drawRect('ground',0,0,0, *self.GROUND)]
-        for i,o in enumerate(self.OBSTACLES):
-            ma.markers.append(self.drawRect('obs',i,*o))
-        for i, (x,y,th) in enumerate(self.AR_TAGS):
-            ma.markers.append(self.drawRect('ar', i, x,y,.1,.1))
-        self.map_pub.publish(ma)
-    
+
+          
 
 if __name__=="__main__":
     rospy.init_node("global_potential_field")
@@ -253,7 +338,10 @@ if __name__=="__main__":
         ]
  
     
-    gpf = GlobalPotentialField(GROUND, OBSTACLES, AR_TAGS)
+    armap = ARMap(GROUND, OBSTACLES, AR_TAGS)
+    gpf = GlobalPotentialField(armap)
+    cc = CarControl(gpf)
+
     rospy.spin()
 
 
--- src/bwsi/ground_truth.py
+++ src/bwsi/ground_truth.py
@@ -5,17 +5,22 @@
 
 
 import rospy
+from std_msgs.msg import Header
 from gazebo_msgs.msg import ModelStates
 import tf
 from convert_functions import pose_to_pos_and_quat
 
+from geometry_msgs.msg import Pose, PoseStamped, Vector3
 
 class Odom:
     def __init__(self):
         self.name = 'racecar'
         self.link_from = 'base_link'
-        self.link_to = "odom"
+        self.link_to = "map"
         self.br = tf.TransformBroadcaster()
+
+        self.pub_car= rospy.Publisher("mle_pose", \
+                PoseStamped, queue_size=1)
         self.sub = rospy.Subscriber(
             "/gazebo/model_states", ModelStates, self.cb, queue_size=1)
 
@@ -26,8 +31,15 @@ class Odom:
         for i in range(len(data.name)):
             if self.name == data.name[i]:
                 link_from = self.link_from
+
+                p = PoseStamped()
+                p.header =  Header(0, rospy.Time.now(), 'map')
+                p.pose = data.pose[i]
+
+                self.pub_car.publish(p)
             else:
                 link_from = data.name[i]
+
             self.broadcast(data.pose[i], time, link_from, link_to)
 
     def broadcast(self, pose, time, link_from, link_to):
